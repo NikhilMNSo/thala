1720eacb815e500f3854c4dcc166c88fa4db28c3
M Nikhil <155064830+NikhilMNSo@users.noreply.github.com>
2024-08-26 19:51:54 +0530

    thala.java

--- thala.java
+++ thala.java
@@ -5,15 +5,46 @@ import java.util.Scanner;
 
 public class thala {
 
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the name: ");
 
-        String userInput = scanner.nextLine();
+        String msd = scanner.nextLine();
 
 
         scanner.close();
-        System.out.println(userInput);
-        System.out.println("thala for a reason");
-    }
+        if (msd.length() == 7) 
+        {
+            for(int i = 0; i<msd.length(); i++)
+            {
+                System.out.print(msd.charAt(i));
+                if(i<msd.length() - 1)
+                {
+                    System.out.print("+");
+                    
+                }
+                 
+               
+            }
+             System.out.print("="+msd.length()+" Thala for a reason");
+           
+        }
+        else
+        {
+            for(int i = 0; i<msd.length(); i++)
+            {
+                System.out.print(msd.charAt(i));
+                if(i<msd.length() - 1)
+                {
+                    System.out.print("+");
+                    
+                }
+                
+            }
+            System.out.print("="+msd.length()+" Thala for No reason");
+        }
+            
+
+}
 }
